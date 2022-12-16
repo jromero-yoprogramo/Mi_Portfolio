d60f1cc83925da0cd09a10327098203d4a099fc3
jromero-yoprogramo <jromero.yoprogramo@gmail.com>
2022-12-16 15:33:34 -0300

    Hice Routing para el proyecto y el login (con el boton)

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -14,6 +14,8 @@ import { HyssComponent } from './components/hyss/hyss.component';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +28,9 @@ import { HttpClientModule } from '@angular/common/http';
     EducacionComponent,
     HyssComponent,
     ProyectoComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
